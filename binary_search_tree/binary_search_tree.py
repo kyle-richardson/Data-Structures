463e157dd33d1c9842d21152b19d0e07abac1ce4
Kyle Richardson <kyle.richardson1@gmail.com>
2020-07-24 22:50:26 -0500

    remove small test prints

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -313,17 +313,3 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
-
-
-bst = BSTNode(1)
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
-
-bst.in_order_print(bst)
-# bst.bft_print(bst)
-# bst.dft_print(bst)
